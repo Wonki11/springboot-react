786ec55044498b8009090514329c11e8d8371645
Wonki11 <wongi5011@daum.net>
2024-07-26 09:35:22 +0900

    sql 연결

---
+++ Mysql-Gradle-test/src/main/java/com/kh/MysqlGradleTestApplication.java
@@ -0,0 +1,13 @@
+package com.kh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlGradleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlGradleTestApplication.class, args);
+	}
+
+}
